f055dc2af871bfbea2611b467093a6303b983dc1
Kishan1805 <kkhadela99@gmail.com>
2019-06-26 16:14:59 +0530

    initial commit

---
+++ CodeCoordination/CodeCoordination/Coordination.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using CodeCoordination.CodeExecuterService;
+using SupportedLanguage;
+using Errors;
+using System.IO;
+namespace CodeCoordination
+{
+    public class Coordination : ICoordination
+    {
+        CodeExecuterService.ExecuterClient codecli = new ExecuterClient();
+        private string test, casegen = "casegen.txt", codep, codes, opp = "opp.txt", ops = "ops.txt";
+        private language testl, codepl, codesl;
+        public error e;
+        public void execute()
+        {
+            DateTime start = DateTime.Now;
+            while (true)
+            {
+                int f = 0;
+                gentestcase();
+                runp();
+                runs();
+                string line1, line2;
+                string l = "";
+                f = ops.Length;
+                for(int i=0;i<f-3;i=i+1)
+                {
+                    l = l + ops[i];
+                }
+                line1 = File.ReadAllText(@"F:\codefolder\" + opp);
+                line2 = File.ReadAllText(@"F:\codefolder\" + ops);
+                if (line2.Contains(l))
+                {
+                    FileStream File1 = new FileStream(@"F:\codefolder\" + ops + "result", FileMode.OpenOrCreate, FileAccess.Write);
+                    using (StreamWriter sw = new StreamWriter(File1))
+                    {
+                        sw.WriteLine(ops + " have compilation error: ");
+                        sw.WriteLine(line2);
+                    }
+                    File1.Close();
+                    return;
+                }
+                if (line1 != line2)
+                {
+                    FileStream File1 = new FileStream(@"F:\codefolder\"+ops+"result", FileMode.OpenOrCreate, FileAccess.Write);
+                    using (StreamWriter sw = new StreamWriter(File1))
+                    {
+                        sw.WriteLine(ops +" gets a failure on");
+                        string line3 = File.ReadAllText(@"F:\codefolder\casegen.txt");
+                        sw.WriteLine(line3);
+                    }
+                    //File.Delete(@"F:\codefolder\" + opp);
+                    //File.Delete(@"F:\codefolder\" + opp);
+                    File1.Close();
+                    return;
+                }
+                if((DateTime.Now-start).TotalSeconds >=120)
+                {
+                    FileStream File1 = new FileStream(@"F:\codefolder\"+ops+"result", FileMode.OpenOrCreate, FileAccess.Write);
+                    using (StreamWriter sw = new StreamWriter(File1))
+                    {
+                        sw.WriteLine(ops+" gets the success");
+                    }
+                    File1.Close();
+                    return;
+                }
+            }
+        }
+
+        public void gentestcase()
+        {
+            codecli.executeCode(testl, test, null, casegen);
+        }
+
+        public void runp()
+        {
+            codecli.executeCode(codepl, codep, casegen, opp);
+        }
+        public void runs()
+        {
+            codecli.executeCode(codesl, codes, casegen, ops);
+        }
+
+        public void settestl(language lang)
+        {
+            testl = lang;
+        }
+        public void setcodepl(language lang)
+        {
+            codepl = lang;
+        }
+        public void setcodesl(language lang)
+        {
+            codesl = lang;
+        }
+        public void settest(string name)
+        {
+            test = name;
+        }
+        public void setcasegen(string name)
+        {
+            casegen = name;
+        }
+        public void setcodep(string name)
+        {
+            codep = name;
+        }
+        public void setcodes(string name)
+        {
+            codes = name;
+        }
+        public void setopp(string name)
+        {
+            opp = name;
+        }
+        public void setops(string name)
+        {
+            ops = name;
+        }
+    }
+}
---
+++ CodeExecuter/CodeExecuter/CodeExecuter/Executer.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.Diagnostics;
+using System.IO;
+using SupportedLanguage;
+using Errors;
+
+namespace CodeExecuter
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    public class Executer : IExecuter
+    {
+        static string codefolder = @"f:\\codefolder";
+        private static int tlimit = 5000;
+        public error executeCode(language lang, string filename, string input, string output)
+        {
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.WorkingDirectory = codefolder;
+            startInfo.CreateNoWindow = true;
+            startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            startInfo.FileName = "cmd.exe";
+            switch (lang)
+            {
+                case language.cpp : startInfo.Arguments = @"/C g++ -o "+ filename.Substring(0,filename.Length-4) + " " + filename + " 2> " + output ;
+                    Process.Start(startInfo).WaitForExit();
+                    string cc = File.ReadAllText(codefolder + @"\"+ output);
+                    if (cc.Length != 0)
+                    {
+                        return error.CE;
+                    }
+                    startInfo.Arguments = @"/C " + filename.Substring(0, filename.Length - 4);
+                    if (input != null)
+                        startInfo.Arguments += " < " + input;
+                    if(output != null)
+                        startInfo.Arguments+=" > " +output;
+                    var proc=Process.Start(startInfo);
+                    bool re=proc.WaitForExit(tlimit);
+                    if (!re)
+                    {
+                        //foreach (var pro in Process.GetProcessesByName(filename.Substring(0, filename.Length - 4)))
+                            //pro.Kill();
+                        return error.RE;
+                    }
+                    //foreach (var pro in Process.GetProcessesByName(filename.Substring(0, filename.Length - 4)))
+                       // pro.Kill();
+                    break;
+            }
+            return error.AC;
+        }
+    }
+}
